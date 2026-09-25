c77a761c048cf57d09f9b54e090ab6531aa03e4f
Joan Dongmo <joanfreeddonfmomeufo@gmail.com>
2026-09-25 20:24:33 +0100

    fix: changement du montant Campay Demo à 10 XAF

--- lib/services/service_paiement.dart
+++ lib/services/service_paiement.dart
@@ -77,8 +77,8 @@ class ServicePaiement {
         "CAMTRANS-${courseId.substring(0, 5).toUpperCase()}-${DateTime.now().millisecondsSinceEpoch}";
 
     // L'API Demo de Campay n'accepte pas les transactions de plus de 25 FCFA.
-    // Pour la soutenance, on envoie 5 FCFA à Campay, mais on enregistre le vrai prix dans Firebase.
-    final double montantCampay = ApiKeys.isCampayProduction ? montant : 5.0;
+    // Pour la soutenance, on envoie 10 FCFA à Campay, mais on enregistre le vrai prix dans Firebase.
+    final double montantCampay = ApiKeys.isCampayProduction ? montant : 10.0;
 
     final collectResponse = await http.post(
       Uri.parse('$_baseUrl/collect/'),
